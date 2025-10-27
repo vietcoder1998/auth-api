c0f7185aee585101b3cd79a21dc01b38e7048a7f
viego.tran <viego.tran@techvify.com.vn>
2025-10-27 13:47:04 +0700

    feat: Update ToolCommandSeeder to fix type definitions and improve command seeding logic

--- prisma/seeders/tool-command.seeder.ts
+++ prisma/seeders/tool-command.seeder.ts
@@ -1,16 +1,43 @@
-import { prisma } from '../../src/setup';
+import { CommandDro, CommandModel, ToolDto } from '../../src/interfaces';
 import { CommandRepository } from '../../src/repositories/command.repository';
 import { ToolRepository } from '../../src/repositories/tool.repository';
-import { CommandDto, ToolDro } from '../../src/interfaces';
+import { prisma } from '../../src/setup';
 
 class ToolCommandSeeder {
   private toolRepository = new ToolRepository(prisma.tool);
   private commandRepository = new CommandRepository(prisma.command);
+  public instance = new ToolCommandSeeder();
 
   async seed() {
-    const tools: ToolDro = await this.toolRepository.findByLabel('mock')
+    const tools: ToolDto[] | null = await this.toolRepository.findByLabel('mock')
+    if (!tools) {
+        throw new Error("No tools found with label 'mock'");
+    }
+    const commands: CommandModel[] = tools.map(tool => {
+        const now = new Date();
+        return {
+            id: '', // Provide a default or generated id as needed
+            name: 'CREATE MISSING PERMISSION', // Provide a default name
+            action: 'POST', // Provide a default action
+            enabled: true, // Provide a default enabled value
+            toolId: tool.id,
+            command: 'command1',
+            repository: 'permission',
+            params: `{
+                name: 'Create permission',
+                route: '/api/permission',
+                method: 'POST'
+            }`,
+            createdAt: now,
+            updatedAt: now,
+            // Add any other required properties with default/mock values
+        }
+    })
 
+    const toolSeeds =  this.commandRepository.bulkCreate(commands)
 
-
-        this.commandRepository.bulkCreate(coomand)
+    return toolSeeds
+  }
 }
+
+export default ToolCommandSeeder;
