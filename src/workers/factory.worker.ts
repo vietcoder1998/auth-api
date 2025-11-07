84cc5d5f6f17cb952082ef37b8c72bad538ad7d5
viego.tran <viego.tran@techvify.com.vn>
2025-11-07 10:13:49 +0700

    feat: Enhance Thread and ThreadWorker classes with job management and worker state tracking

--- src/workers/factory.worker.ts
+++ src/workers/factory.worker.ts
@@ -1,24 +1,99 @@
+import { Job, JobErrorResult, JobResult, JobSuccessResult } from './job.worker';
 import { ThreadWorker } from './thread.worker';
-import {Job, JobSuccessResult, JobErrorResult} from './job.worker'
 
 export class Thread {
-    private readonly threadId: string;
-    public readonly jobList: Job[] = []
-    private jobSuccessList: JobSuccessResult[] = []
-    private jobErrorList: JobErrorResult[] = []
-    private readonly threadWorkers: ThreadWorker[] = []
+  private readonly threadId: string;
+  public readonly jobList: Job[] = [];
+  private jobSuccessList: JobSuccessResult[] = [];
+  private jobErrorList: JobErrorResult[] = [];
+  private readonly threadWorkers: ThreadWorker[] = [];
 
-    public constructor(threadId: string, jobList: Job[], threadWorkers: ThreadWorker[]) {
-        this.threadId = threadId
-        this.jobList = jobList
-        this.threadWorkers = threadWorkers
+  public constructor(threadId: string, jobList: Job[], threadWorkers: ThreadWorker[]) {
+    this.threadId = threadId;
+    this.jobList = jobList;
+    this.threadWorkers = threadWorkers;
+  }
+
+  public start(): ThreadWorker[] {
+    for (
+      let threadWorkerIndex = 0;
+      threadWorkerIndex < this.threadWorkers.length;
+      threadWorkerIndex++
+    ) {
+      const threadWorker: ThreadWorker = this.threadWorkers[threadWorkerIndex];
+
+      threadWorker.on('message', (message: string, job: Job) => {
+        // Handle messages from the worker
+        this.checkExitStatus();
+      });
+      threadWorker.on('error', (error) => {
+        // Handle errors from the worker
+        const jobResult = new JobErrorResult(threadWorker?.job?.jobId || '', {});
+        this.sendJobErrorResult(jobResult);
+        this.checkExitStatus();
+      });
+      threadWorker.on('exit', (code) => {
+        // Handle worker exit
+        threadWorker.inActive();
+        this.removeThreadWorker(threadWorker.workerId);
+        this.checkExitStatus();
+      });
     }
+
+    return this.threadWorkers;
+  }
+  public cancel(): JobResult[] {
+    const results: JobResult[] = [];
+
+    return results;
+  }
+  public addNewThreadWorker(threadWorker: ThreadWorker): ThreadWorker[] {
+    this.threadWorkers.push(threadWorker);
+
+    return this.threadWorkers;
+  }
+  public removeThreadWorker(threadWorkerId: string): ThreadWorker[] {
+    const index = this.threadWorkers.findIndex((tw) => tw.workerId === threadWorkerId);
+
+    if (index > -1) {
+      this.threadWorkers.splice(index, 1);
+    }
+    return this.threadWorkers;
+  }
+  public sendJobErrorResult(result: JobResult): void {
+    this.jobErrorList.push(result as JobErrorResult);
+  }
+  public sendJobSuccessResult(result: JobResult): void {
+    this.jobSuccessList.push(result as JobSuccessResult);
+  }
+  public countActiveWorkers(): number {
+    return this.threadWorkers.filter((threadWorker: ThreadWorker) => threadWorker.active).length;
+  }
+  public removeAllWorker(): ThreadWorker[] {
+    this.threadWorkers.splice(0, this.threadWorkers.length);
+    return this.threadWorkers;
+  }
+  public removeAllJobs(): Job[] {
+    this.jobList.splice(0, this.jobList.length);
+    return this.jobList;
+  }
+  public close(): void {
+    this.removeAllJobs();
+    this.removeAllWorker();
+  }
+  private checkExitStatus(): void {
+    if (
+      !this.countActiveWorkers() ||
+      this.jobList.length === this.jobSuccessList.length + this.jobErrorList.length
+    ) {
+      return this.close();
+    }
+  }
 }
-
 export class Factory {
-    private readonly thread: Thread
+  private readonly thread: Thread;
 
-    public constructor(thread: Thread) {
-        this.thread = thread
-    }
+  public constructor(thread: Thread) {
+    this.thread = thread;
+  }
 }
--- src/workers/thread.worker.ts
+++ src/workers/thread.worker.ts
@@ -1,7 +1,37 @@
 import { BaseWorker } from './base.worker';
+import { Job } from './job.worker';
 
 interface BackupResult {}
 
 export class ThreadWorker extends BaseWorker<BackupResult> {
-    
+  private _active: number = 1;
+  private _position: string = 'cpu';
+  private _job: Job | null = null;
+
+  public set job(value: Job | null) {
+    this._job = value;
+  }
+
+  public get job(): Job | null {
+    return this._job;
+  }
+
+  constructor(filename: string, options?: WorkerOptions) {
+    super(filename, options);
+  }
+  public get active(): boolean {
+    return this._active > 0;
+  }
+  public inActive(): boolean {
+    this._active = 0;
+
+    return this._active <= 0;
+  }
+  public get position() {
+    return this._position;
+  }
+
+  public set position(pos: string) {
+    this._position = pos;
+  }
 }
