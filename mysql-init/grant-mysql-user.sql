440fafd6acb64ccce1bd2a621f9c63d496e2b70b
Trần Duy Viêt <duyviet2841998@gmail.com>
2025-11-12 14:25:43 +0700

    feat: Update MySQL user credentials to use admin_user and remove devuser references

--- mysql-init/grant-mysql-user.sql
+++ mysql-init/grant-mysql-user.sql
@@ -1,4 +1,5 @@
 -- grant-mysql-user.sql
--- This script grants all privileges to devuser from any host for local development
-GRANT ALL PRIVILEGES ON *.* TO 'devuser'@'%' IDENTIFIED BY 'devuser_password';
+-- This script grants all privileges to admin_user from any host for local development
+CREATE USER IF NOT EXISTS 'admin_user'@'%' IDENTIFIED BY 'admin_password';
+GRANT ALL PRIVILEGES ON *.* TO 'admin_user'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
